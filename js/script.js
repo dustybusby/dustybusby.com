fc9f6ae4208e38a82976c2b76fb4b08c81eaf2f1
Dusty Busby <dusty.busby@pm.me>
2024-11-26 15:26:03 -0600

    first commit after new domain name

---
+++ js/script.js
@@ -0,0 +1,5 @@
+document.querySelector('.menu-toggle').addEventListener('click', function() {
+    document.querySelector('.nav').classList.toggle('active');
+    document.querySelector('.menu-toggle').classList.toggle('active');
+    document.querySelector('.content').classList.toggle('active');
+});
