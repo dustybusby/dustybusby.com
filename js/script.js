d3af67af0889aecb703d471a8da574b4ec461681
Dusty Busby <dusty.busby@pm.me>
2024-12-10 16:43:05 -0600

    updated code with sunset

--- js/script.js
+++ js/script.js
@@ -1,3 +1,4 @@
+// Web Designer: Dusty Busby
 document.querySelector('.menu-toggle').addEventListener('click', function() {
     document.querySelector('.nav').classList.toggle('active');
     document.querySelector('.menu-toggle').classList.toggle('active');
